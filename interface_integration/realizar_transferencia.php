1b569df60ad82e2f07d5247d1e3067d9990f4f42
Derivatto <carlos3x5123@gmail.com>
2022-12-20 15:21:23 -0300

    Integração do modal de transferencia e alimentação do banco de dados

--- interface_integration/realizar_transferencia.php
+++ interface_integration/realizar_transferencia.php
@@ -9,7 +9,8 @@ error_reporting(E_ALL);
 
     require_once "../dbmanager/connectdb.php";
     require_once "../models/Conta.class.php";
-    
+    require_once "../models/Transferencia.class.php";
+
     session_start();
     $agenciaDestino = $_POST['agenciaDestino'];
     $numContaDestino = $_POST['contaDestino'];
@@ -17,6 +18,10 @@ error_reporting(E_ALL);
     $valor = $_POST['valor'];
     $contaRemetente = $_SESSION['idConta'];
 
+    echo date('m-d-Y');
+    Transferencia::create($connection, $contaRemetente, date('Y-m-d'), $valor, $contaRemetente, $contaDestino);
+    Transferencia::create($connection, $contaDestino, date('Y-m-d'), $valor, $contaDestino, $contaRemetente);
+    
     Conta::realizarTransferencia($connection, $contaRemetente, $contaDestino, $valor);
     header("Location: ../interface/dashboard.php");
 ?>
